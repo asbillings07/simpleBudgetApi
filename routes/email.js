8ec714a8d73824401388256596afd4eb8e6b05d8
asbillings07 <superswiftdeveloper@gmail.com>
2021-03-15 10:41:47 -0400

    removed unneded routes

--- routes/email.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-require('dotenv').config()
-const router = express.Router()
-const { confirmSpeaker } = require('../controllers/emailController')
-
-router.post('/sendEmail', confirmSpeaker)
-
-module.exports = router
--- routes/text.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-require('dotenv').config()
-const router = express.Router()
-const { sendTextMessage, respondToTextMessage } = require('../controllers/textController')
-
-router.post('/sendSms', sendTextMessage)
-router.post('/replySms', respondToTextMessage)
-module.exports = router
